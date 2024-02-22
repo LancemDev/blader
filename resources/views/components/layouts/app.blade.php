f1d4c4b089d53c50ce53ebcb8d9c0010a420e5a0
LancemDev <munyaolance1@gmail.com>
2024-02-22 06:03:06 +0300

    Feat: did the modal for video upload

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,6 +6,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ isset($title) ? $title.' - '.config('app.name') : config('app.name') }}</title>
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.6.1/cropper.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.6.1/cropper.min.css" />
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 
@@ -18,7 +21,7 @@
             <x-mary-input placeholder="Search..." wire:model.live.debounce="search" clearable icon="o-magnifying-glass" />
         </x-slot:middle>
         <x-slot:actions>
-            <x-mary-button label="Upload Video" onclick="modal18.showModal()" @click="$wire.drawer = true" responsive icon="o-funnel" class="btn-primary" />
+            <x-mary-button label="Upload Video" onclick="modal17.showModal()" @click="$wire.drawer = true" responsive icon="o-funnel" class="btn-primary" />
         </x-slot:actions>
     </x-mary-header>
 </head>
@@ -56,7 +59,6 @@
                 <x-mary-menu-sub title="Settings" icon="o-cog-6-tooth">
                     @livewire('logout')
                     <x-mary-menu-item title="Logout" icon="o-wifi" wire:click="logout" />
-                    
                 </x-mary-menu-sub>
             </x-mary-menu>
         </x-slot:sidebar>
@@ -77,17 +79,14 @@
             <x-mary-button label="Cancel" onclick="modal17.closeModal()" class="btn-ghost" />
             <x-mary-button label="Upload" onclick="modal17.closeModal()" class="btn-primary" />
         </x-slot:actions>
-        <x-mary-input placeholder="Title" />
-        <x-mary-input placeholder="Description" />
-        <x-mary-input placeholder="Tags" />
-        <x-mary-input placeholder="Video" />
-    </x-mary-modal>
-
-    <x-mary-modal id="modal18" class="p-5 items-center">
-        <label for="file-upload" class="cursor-pointer">
-            <span class="mt-2 text-white">Upload Video or Drag and Drop here</span>
-        </label>
-        <input wire:model="file" id="file-upload" type="file" class="hidden" />
+        <br />
+        <x-mary-input label="Title" placeholder="Title" />
+        <br />
+        <x-mary-input label="Description" placeholder="Description" />
+        <br />
+        <x-mary-input label="Tags" placeholder="Tags" />
+        <br />
+        <x-mary-input label="Video" placeholder="Video" />
     </x-mary-modal>
 </body>
 </html>
